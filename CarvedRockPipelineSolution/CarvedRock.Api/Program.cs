3bbab2c416d12e9a45ed3fb005ae508095e2c4dd
pauljmartin16 <pauljmartin16@gmail.com>
2022-01-21 13:27:09 -0500

    CR-1 Initial Commit

---
+++ CarvedRockPipelineSolution/CarvedRock.Api/Program.cs
@@ -0,0 +1,51 @@
+using CarvedRock.Api.Domain;
+using CarvedRock.Api.Interfaces;
+using CarvedRock.Api.Middleware;
+using Serilog;
+using Serilog.Events;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog();
+
+var name = typeof(Program).Assembly.GetName().Name;
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .Enrich.WithMachineName()
+    .Enrich.WithProperty("Assembly", name)
+    .WriteTo.Seq(serverUrl: "http://ingestion.seq.mydomain.com")
+    .WriteTo.Console()
+    .CreateLogger();
+
+// Add services to the container.
+// pjm - start
+builder.Services.AddScoped<IProductLogic, ProductLogic>();
+builder.Services.AddScoped<IQuickOrderLogic, QuickOrderLogic>();
+// pjm - end
+
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseMiddleware<GlobalErrorHandlerMiddleware>()
+   .UseSerilogRequestLogging();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
